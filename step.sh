b7a4ed00c6d1f02ae432d324509fc4053821e324
RashmiPad <39586491+RashmiPad@users.noreply.github.com>
2018-05-31 15:49:34 +0530

    Changed the "BUILD_DIR" path and the fixture

--- step.sh
+++ step.sh
@@ -34,12 +34,12 @@ echo "${ANDROID_PROJECT_FOLDER}"
 
 
 ARTIFACTS_DIR="Artifacts"
-BUILD_DIR="Petco.UITests/bin/Release"
+BUILD_DIR="Tasky.UITests/bin/Debug"
 MANIFEST_PATH="${ARTIFACTS_DIR}/manifest.json"
 npm install appcenter-cli@1.0.8 -g
 msbuild "Petco.UITests/Petco.UITests.csproj" /p:Configuration=Release
 
 
-appcenter test prepare uitest --artifacts-dir "${ARTIFACTS_DIR}" --app-path "${app_path}" --build-dir "${BUILD_DIR}" --fixture "Petco.UITests.SmokeTest(Android)" --fixture "Petco.UITests.TheStoreLocator(Android)" --debug --quiet
+appcenter test prepare uitest --artifacts-dir "${ARTIFACTS_DIR}" --app-path "${app_path}" --build-dir "${BUILD_DIR}" --fixture "Tasky.UITests.Tests(Android)" --debug --quiet
 
-appcenter test run manifest --manifest-path "${MANIFEST_PATH}" --app "${app_center_app}" --devices 6f2c8184 --test-series "master" --locale "en_US" --debug --quiet --token "${app_center_token}" --async
+appcenter test run manifest --manifest-path "${MANIFEST_PATH}" --app "${app_center_app}" --devices 3d930114 --test-series "master" --locale "en_US" --debug --quiet --token "${app_center_token}" --async
