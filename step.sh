30e24c5e161b0eb847a695479f8aee3f8b37100f
RashmiPad <39586491+RashmiPad@users.noreply.github.com>
2018-06-05 15:46:08 +0530

    Changes UITest Project and added Android project

--- step.sh
+++ step.sh
@@ -31,13 +31,14 @@ echo " (i) Provided app center app: ${app_center_app}"
 echo " (i) Provided app center token: 4f2a6ef04d856c91ce75e01adf03f5af032de65c"
 echo "${ANDROID_PROJECT_FOLDER}"
 
-
+msbuild "TaskyAndroid/TaskyAndroid.csproj" /t:Clean /p:Configuration=Debug
+msbuild "TaskyAndroid/TaskyAndroid.csproj" /t:PackageForAndroid /p:Configuration=Debug
 
 ARTIFACTS_DIR="Artifacts"
 BUILD_DIR="Tasky.UITests/bin/Debug"
 MANIFEST_PATH="${ARTIFACTS_DIR}/manifest.json"
 npm install appcenter-cli@1.0.8 -g
-msbuild "Petco.UITests/Petco.UITests.csproj" /p:Configuration=Release
+msbuild "Tasky.UITests/Tasky.UITests.csproj" /p:Configuration=Debug
 
 
 appcenter test prepare uitest --artifacts-dir "${ARTIFACTS_DIR}" --app-path "${app_path}" --build-dir "${BUILD_DIR}" --fixture "Tasky.UITests.Tests(Android)" --debug --quiet
